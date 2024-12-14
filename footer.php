afd891fc43430f177e62acf7daf9552d29fe51e4
liftlobby <ai220259@student.uthm.edu.my>
2024-12-14 23:42:57 +0800

    first commit

---
+++ footer.php
@@ -0,0 +1,31 @@
+<footer class="footer">
+    <div class="footer-content">
+        <div class="footer-section">
+            <h3>Location of HQ</h3>
+            <p>Jalan Sultan Hishamuddin,<br>50621 Wilayah Persekutuan,<br>Kuala Lumpur.</p>
+        </div>
+        <div class="footer-section">
+            <h3>Call Center</h3>
+            <p> +603-9779 1200</p>
+        </div>
+        <div class="footer-section">
+            <h3>Operation Time</h3>
+            <p>Everyday: 7:00 AM - 10:00 PM</p>
+        </div>
+        <div class="footer-section">
+            <h3>Email</h3>
+            <p>callcenter@ktmb.com.my</p>
+        </div>
+        <div class="footer-section">
+            <h3>Follow Us</h3>
+            <a href="#"><img src="image\social media\fb icon.png" alt="Facebook"></a>
+            <a href="#"><img src="image\social media\X icon.png" alt="Twitter"></a>
+            <a href="#"><img src="image\social media\Instagram_icon.png" alt="Instagram"></a>
+        </div>
+        <div class="footer-section">
+            <h3>Links</h3>
+            <a href="about_us.php">About Us</a><br>
+            <a href="staff_login.php">Staff Login</a>
+        </div>
+    </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Railway Ticketing and Reservation System for KTM</title>
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+
+    <div class="index-content">
+        <h1>Welcome to KTM Ticketing and Reservation System</h1>
+        <h2>Have A Safe Journey With Us</h2>
+        <br/><br/><br/>
+        <button onclick="window.location.href='reservation.php'">Buy tickets</button>
+
+        <p>Does not have an account? <a href="register.php">Register Now!</a></p>
+    </div>
+
+    <?php include 'footer.php'; ?>
+</body>
+
+</html>
